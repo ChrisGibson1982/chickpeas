d1aba89b66a83944151dedd8fbc3fcff9c626ebd
Chris Gibson <cgfootman@hotmail.com>
2020-03-07 22:23:43 +0000

    add get docs code

--- getMembers/run.ps1
+++ getMembers/run.ps1
@@ -8,9 +8,23 @@ Write-Host "PowerShell HTTP trigger function processed a request."
 
 $primaryKey = ConvertTo-SecureString -String $env:cDBConnectionString -AsPlainText -Force
 
-$cosmosDbContext = New-CosmosDbContext -Account $env:cDBAccount -Database $env:cDBDatabase -Key $primaryKey
+
+$cosmosDbContext = New-CosmosDbContext -Emulator -Database $env:cDBDatabase -URI 'myemulator.local' -Key $primaryKey
+# $cosmosDbContext = New-CosmosDbContext -Account $env:cDBAccount -Database $env:cDBDatabase -Key $primaryKey
+
+$docQuery = "SELECT * FROM c"
+
+$ResponseHeader = $null
+$documents = Get-CosmosDbDocument -Context $cosmosDbContext -CollectionId 'member' -MaxItemCount 5 -ResponseHeader ([ref] $ResponseHeader) -Query $docQuery
+$continuationToken = [String] $ResponseHeader.'x-ms-continuation'
+
+foreach($doc in $documents){
+
+    Write-Output "Doc: "
+    Write-Output $doc
 
 
+}
 
 
 # Associate values to output bindings by calling 'Push-OutputBinding'.
